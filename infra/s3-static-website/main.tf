053429b42270e44bee32c80f5381c51bbcce95e2
Alison Souza <alisonmsouza@yahoo.com.br>
2025-03-02 19:45:23 -0300

    with warning

--- infra/s3-static-website/main.tf
+++ infra/s3-static-website/main.tf
@@ -15,7 +15,7 @@ variable "bucket_name" {
     type = string
 }
 
-resource "aws_s3_bucket_website_configuration" "static_site_bucket" {
+/* resource "aws_s3_bucket_website_configuration" "static_site_bucket" {
   bucket = "static-site-${var.bucket_name}"
 
   index_document {
@@ -26,24 +26,24 @@ resource "aws_s3_bucket_website_configuration" "static_site_bucket" {
     key = "error.html"
   }
 
+} */
+
+resource "aws_s3_bucket" "static_site_bucket" {
+    bucket = "static-site-${var.bucket_name}"#
+
+    website {
+        index_document = "index.html"
+        error_document = "404.html"
+    }
+
+    tags = {
+      Name = "Static Site Bucket"
+      Environment = "Production"
+    }
 }
 
-#resource "aws_s3_bucket" "static_site_bucket" {
-#    bucket = "static-site-${var.bucket_name}"#
-
-#    website {
-#        index_document = "index.html"
-#        error_document = "404.html"
-#    }
-
-#    tags = {
-#      Name = "Static Site Bucket"
-#      Environment = "Production"
-#    }
-#}
-
 resource "aws_s3_bucket_public_access_block" "static_site_bucket" {
-  bucket = aws_s3_bucket_website_configuration.static_site_bucket.id
+  bucket = aws_s3_bucket.static_site_bucket.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -57,12 +57,12 @@ resource "aws_s3_bucket_acl" "static_site_bucket" {
     aws_s3_bucket_ownership_controls.static_site_bucket
    ]
 
-   bucket = aws_s3_bucket_website_configuration.static_site_bucket.id
+   bucket = aws_s3_bucket.static_site_bucket.id
    acl    = "public-read"
 }
 
 resource "aws_s3_bucket_ownership_controls" "static_site_bucket" {
-  bucket = aws_s3_bucket_website_configuration.static_site_bucket.id
+  bucket = aws_s3_bucket.static_site_bucket.id
 
   rule {
     object_ownership = "BucketOwnerPreferred"
