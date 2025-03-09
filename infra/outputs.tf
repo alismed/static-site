79831d27121682dbdf021346000d56d797890db3
Alison Souza <alisonmsouza@yahoo.com.br>
2025-03-09 16:06:34 -0300

    variables

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -10,6 +10,18 @@ output "bucket_name" {
   value = aws_s3_bucket.static_site_bucket.bucket
 }
 
+output "website_domain" {
+  value = aws_s3_bucket.static_site_bucket.website_domain
+}
+
+output "website_hosting" {
+  value = aws_s3_bucket.static_site_bucket.website_endpoint
+}
+
 output "website_endpoint" {
   value = aws_s3_bucket_website_configuration.static_site.website_endpoint
+}
+
+output "website_url" {
+  value = "http://${aws_s3_bucket.static_site_bucket.website_endpoint}"
 }
--- infra/terraform.tfvars
+++ infra/terraform.tfvars
@@ -1,4 +1,4 @@
 bucket_name    = "my-site-alismed"
 profile        = "default"
 region         = "us-east-1"
-destroy_bucket = true
+destroy_bucket = false
