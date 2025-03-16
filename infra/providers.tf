550c3770a509e3ff54db75e3b73f9ee0906e1769
Alison Souza <alisonmsouza@yahoo.com.br>
2025-03-15 22:12:04 -0300

    cloudfront

--- infra/providers.tf
+++ infra/providers.tf
@@ -10,16 +10,4 @@ terraform {
 provider "aws" {
   region  = var.region
   profile = var.profile
-
-  dynamic "endpoints" {
-    for_each = var.use_localstack ? [1] : []
-    content {
-      s3 = "http://localhost:4566"
-    }
-  }
-
-  skip_credentials_validation = var.use_localstack
-  skip_metadata_api_check     = var.use_localstack
-  skip_requesting_account_id  = var.use_localstack
-  s3_use_path_style           = var.use_localstack
 }
